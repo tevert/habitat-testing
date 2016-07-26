a25a874aacd53c86d487006d772241ffb96a0a0a
tyler.evert <tyler.evert@centare.com>
2016-07-26 13:40:02 -0500

    Initial commit of cookbook

---
+++ cookbooks/myserver/metadata.rb
@@ -0,0 +1,7 @@
+name             'myserver'
+maintainer       'centare'
+maintainer_email 'tevert@centare.com'
+license          'MIT'
+description      'Installs/Configures myserver'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
